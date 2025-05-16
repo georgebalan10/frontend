5c12e9bc7cfb27c74330d1329af5af016c786b69
georgebalan10 <116486631+georgebalan10@users.noreply.github.com>
2025-05-16 20:31:27 +0300

    Fix redirectionare login cu useNavigate

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import API_BASE_URL from "../config";
+import { useNavigate } from 'react-router-dom'; // ✅ Adăugat
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate(); // ✅ Adăugat
 
   const handleSubmit = async (e) => {
     e.preventDefault();
